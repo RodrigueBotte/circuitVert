ffd5bc61d65aed6a4fef22eee45d3d4a348220b6
unknown <rodrigue.botte@gmail.com>
2025-09-30 13:16:14 +0200

    mise en place du register controller pour pro et user avec test fonctionnel sur postman

--- backend/migrations/Version20250930103544.php
+++ backend/migrations/Version20250930103544.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250929124053 extends AbstractMigration
+final class Version20250930103544 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20250929124053 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user ADD type VARCHAR(20) NOT NULL, ADD nom VARCHAR(255) DEFAULT NULL, ADD sirret VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE user ADD is_verified TINYINT(1) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user DROP type, DROP nom, DROP sirret');
+        $this->addSql('ALTER TABLE user DROP is_verified');
     }
 }
---
+++ backend/src/Controller/Api/ApiRegisterController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ApiRegisterController extends AbstractController
+{
+    #[Route('/api/register', name: 'app_api_register')]
+    public function register(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $em): Response
+    {
+        // We receipe the JSON send by the frontend
+        $data = json_decode($request->getContent(), true);
+
+        // We check that the required fields are there
+        if (!isset($data['email'], $data['password'], $data['type'])) {
+            return $this->json(['error' => 'Missing fields'], 400);
+        }
+
+        // We check if the email already exist
+        $existingUser = $em->getRepository(User::class)->findOneBy(['email'=> $data['email']]);
+        if ($existingUser) {
+            return $this->json(['error' => 'Email already used'], 400);
+        }
+
+        // We create a new user
+        $user = new User();
+        $user->setEmail($data['email']);
+        $user->setType($data['type']);
+
+        // Attribution automatily of the role
+        if ($data['type'] === 'pro') {
+            $user->setNom($data['nom']);
+            $user->setSirret($data['sirret']);
+            $user->setRoles(['ROLE_PRO']);
+        }else{
+            $user->setRoles(['ROLE_USER']);
+        }
+
+        // Hash of the password
+        $user->setPassword($passwordHasher->hashPassword($user, $data['password']));
+
+        // Save the user in the database
+        $em->persist($user);
+        $em->flush();
+
+        return $this->json([
+            'status'=> 'success',
+            'messages'=> 'User created successully',
+        ], 201);
+    }
+}
