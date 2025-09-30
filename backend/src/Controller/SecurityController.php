0b128b783f7307aaa2788ea1dd7a3c7001ee4bac
unknown <rodrigue.botte@gmail.com>
2025-09-30 17:00:41 +0200

    tentative de mise en place de login_check

---
+++ backend/src/Controller/SecurityController.php
@@ -0,0 +1,16 @@
+<?php
+// src/Controller/SecurityController.php
+// namespace App\Controller;
+
+// use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+// use Symfony\Component\Routing\Annotation\Route;
+
+// class SecurityController extends AbstractController
+// {
+//     #[Route('/api/login_check', name: 'api_login_check', methods: ['POST'])]
+//     public function loginCheck()
+//     {
+//         // Cette méthode ne sera jamais appelée car json_login intercepte la route
+//         throw new \Exception('This should never be called directly.');
+//     }
+// }
